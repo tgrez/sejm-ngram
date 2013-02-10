f18dcd4e0ff6004df804d23906921b1bd8934c0d
tgrez <tomasz.guz@gmail.com>
2013-02-10 18:31:47 +0100

    added column

--- db/scripts/create_db.sql
+++ db/scripts/create_db.sql
@@ -7,11 +7,12 @@ USE `wystapienia`;
 GRANT ALL ON wystapienia.* TO 'testuser'@'localhost';
 
 CREATE TABLE `ngram` (
-	`id`			INT(10) NOT NULL AUTO_INCREMENT,
-	`unigram`		VARCHAR(20),
-	`posel` 		VARCHAR(40),
-	`data` 			DATE,
-	`partia`		VARCHAR(10),
+	`id`			    INT(10) NOT NULL AUTO_INCREMENT,
+	`unigram`		    VARCHAR(20),
+	`posel` 		    VARCHAR(40),
+	`data` 			    DATE,
+    `partia`            VARCHAR(10),
+	`stanowisko`		VARCHAR(50),
 	`wystapienie_id` 	INT(10),
 	PRIMARY KEY(id)
 	);
