161b924f66892ad40e0a637ab098015b07fffe22
krotny <krotny@gmail.com>
2013-03-11 18:51:57 +0100

    Full names for logging methods.

--- backend/src/log.py
+++ backend/src/log.py
@@ -14,6 +14,15 @@ def err(txt):
     if not txt.endswith("\n"): txt = txt+"\n"
     sys.stderr.write( "[ERR]["+str(inspect.stack()[1][3])+"]"+str(txt) )
 
+def error(txt):
+    if not txt.endswith("\n"): txt = txt+"\n"
+    sys.stderr.write( "[ERR]["+str(inspect.stack()[1][3])+"]"+str(txt) )
+
+
 def warn(txt):
+    if not txt.endswith("\n"): txt = txt+"\n"
+    sys.stderr.write( "[WRN]["+str(inspect.stack()[1][3])+"]"+str(txt) )
+
+def warning(txt):
     if not txt.endswith("\n"): txt = txt+"\n"
     sys.stderr.write( "[WRN]["+str(inspect.stack()[1][3])+"]"+str(txt) )
