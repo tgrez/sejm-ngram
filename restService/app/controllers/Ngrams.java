398b8f1c21158e763fcc1ef201149348ca35d012
msiemionczyk <siemionides@gmail.com>
2014-01-06 17:59:51 +0100

    Set up play framework with localhost:9000/api/ngrams/searches POST method that can be queried with
    curl --header "Content-type: application/json" —request POST --data '{ "dateFrom" : "2012-06-02", "dateTo" : "2012-08-23", "ngram" : "some ngram value" }' http://localhost:9000/api/ngrams/searches
    
    n-joy!

---
+++ restService/app/controllers/Ngrams.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.Logger;
+import play.libs.Json;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by michalsiemionczyk on 06/01/14.
+ */
+public class Ngrams extends Controller {
+
+
+    /**
+     * request should go like:
+     * {
+     *  "dateFrom"    : 2012-06-02,
+     *  "dateTo"      : 2012-08-23,
+     *  "ngram"      : "some ngram value"
+     * }
+     *
+     * and bitches should be like happy.
+     *
+     * response:
+     * {
+     *     "ngram1" :   "some",
+     *     "ngram2" :   "ngram",
+     *     "ngram3" :   "value"
+     * }
+     *
+     * @return
+     */
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result searches(){
+        JsonNode json = request().body().asJson();
+        String ngramValue = json.findPath( "ngram").textValue();
+//        Logger.info( ngramValue);
+
+
+        String[] ngrams = ngramValue.split( " ");
+
+        ObjectNode result = Json.newObject();
+
+        int i = 1;
+        for ( String ngram : ngrams){
+
+             Logger.info( ngram);
+                           result.put("" + i, ngram);
+            i++;
+        }
+
+        return ok (result);
+
+
+    }
+}
---
+++ restService/build.sbt
@@ -0,0 +1,11 @@
+name := "restService"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaEbean,
+  cache
+)     
+
+play.Project.playJavaSettings
