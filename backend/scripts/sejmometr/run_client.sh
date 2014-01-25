d9cf3156e69f55d9d295a8bd147ce29c254fe2dd
tgrez <tomasz.guz@gmail.com>
2014-01-25 17:16:42 +0100

    added scripts to run sejmometr client

---
+++ backend/scripts/sejmometr/run_client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MAIN_PATH=/home/tomasz/workspace/sejmAPI/sejm-ngram/backend
+SEJMOMETR_CLIENT=$MAIN_PATH/sejmometr-client
+SEJMOMETR_JAR=$SEJMOMETR_CLIENT/target/sejmometr-client-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+java -jar $SEJMOMETR_JAR
