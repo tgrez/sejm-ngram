213c44135f65b668ccd5f035b0f0c08ab897de6f
Krzysztof Siejkowski <krzysztof.siejkowski@ext.allegro.pl>
2014-03-01 18:07:08 +0100

    Changes,

--- backend/psc-parser/src/main/java/org/sejmngram/psc/parser/FileLoader.java
+++ backend/psc-parser/src/main/java/org/sejmngram/psc/parser/FileLoader.java
@@ -2,12 +2,17 @@ package org.sejmngram.psc.parser;
 
 import org.apache.commons.io.FileUtils;
 import org.joox.JOOX;
+import org.sejmngram.common.json.JsonProcessor;
+import org.sejmngram.common.json.datamodel.Wystapienie;
+import org.sejmngram.common.json.print.Printer;
 import org.w3c.dom.Element;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Hashtable;
-import java.util.List;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class FileLoader {
 
@@ -16,53 +21,202 @@ public class FileLoader {
     }
 
     private static void parsePSC() {
-        File headerFile = FileUtils.getFile("../psc-data/01-pp-001-01/header.xml");
+
+        int kadencja = 1;
+
+        ArrayList partiesList = readPartiesListFile(kadencja);
+        HashMap<String, OpisPoslaHelper> partie = parsePartiesList(partiesList);
+        HashSet<String> imionaPoslow = getImionaPoslow(partie);
+
+        // TODO: dopisać fragment, ktory chodzi po katalogach i parsuje plik po pliku
+        String header = readHeaderFile();
+        String text = readContentFile();
+        Date date = getDate(header);
+        String baseTitle = getBaseTitle(header);
+        Hashtable<String, String> poslowie = getPoslowie(header);
+
+
+        List<Wystapienie> wystapienies = new ArrayList<Wystapienie>();
+        List<Element> wystapienia = JOOX.$(text).find("body").find("div").get();
+        for (Element wystąpienie : wystapienia) {
+            List<Element> wypowiedzi = JOOX.$(wystąpienie).find("u").get();
+            for (Element wypowiedz : wypowiedzi) {
+                String who = JOOX.$(wypowiedz).attr("who");
+                if (who != null && !who.equalsIgnoreCase("#komentarz")) {
+                    String keyForPoslowie = who.replace("#", "");
+                    String poselFull = poslowie.get(keyForPoslowie);
+                    PoselStanowiskoHelper poselStanowisko = getPoselStanowisko(imionaPoslow, keyForPoslowie, poselFull);
+                    String tresc = getTresc(wypowiedz);
+                    String tytul = getTytul(baseTitle, poselFull);
+                    String partia = getPartia(partie, poselStanowisko.posel);
+                    Wystapienie wystapienie = createWystapienie(date, poselStanowisko.posel,
+                            poselStanowisko.stanowisko, tresc, tytul, partia);
+                    wystapienies.add(wystapienie);
+                }
+            }
+        }
+        Printer.printCommonJsonsToFiles(wystapienies);
+    }
+
+    private static PoselStanowiskoHelper getPoselStanowisko(HashSet<String> imionaPoslow, String keyForPoslowie, String poselFull) {
+        PoselStanowiskoHelper poselStanowisko = new PoselStanowiskoHelper();
+        if (poselFull != null) {
+            String[] poselFullSplitted = poselFull.split("\\ ");
+            int i = 0;
+            for (; i < poselFullSplitted.length; i++) {
+                if (imionaPoslow.contains(poselFullSplitted[i])) break;
+            }
+            if (i == poselFullSplitted.length) { // nie znaleziono imienia
+                poselStanowisko.posel = "";
+                poselStanowisko.stanowisko = poselFull;
+            } else {
+                int indexOfName = poselFull.indexOf(poselFullSplitted[i]);
+                if (indexOfName == 0) { // nie znaleziono stanowiska
+                    poselStanowisko.posel = poselFull;
+                    poselStanowisko.stanowisko = "";
+                } else {
+                    poselStanowisko.stanowisko = poselFull.substring(0, indexOfName);
+                    poselStanowisko.posel = poselFull.substring(indexOfName);
+                }
+            }
+            System.out.println("posel: " + poselStanowisko.posel + ", stanowisko: " + poselStanowisko.stanowisko);
+        } else {
+            poselStanowisko.posel = keyForPoslowie;
+            poselStanowisko.stanowisko = keyForPoslowie;
+        }
+        return poselStanowisko;
+    }
+
+    private static HashSet<String> getImionaPoslow(HashMap<String, OpisPoslaHelper> partie) {
+        HashSet<String> imionaPoslow = new HashSet<String>();
+        for(String imieNazwisko : partie.keySet()) {
+            String imieNazwiskoEnhanced = imieNazwisko.replaceFirst("\\ ", "######");
+            String[] imieNazwiskoArray = imieNazwiskoEnhanced.split("######");
+            String imie = imieNazwiskoArray[0];
+            imionaPoslow.add(imie);
+        }
+        return imionaPoslow;
+    }
+
+    private static String getTresc(Element wypowiedz) {
+        return JOOX.$(wypowiedz).content();
+    }
+
+    private static String getPartia(HashMap<String, OpisPoslaHelper> partie, String posel) {
+        String partia = "";
+        if (partie.get(posel) != null) {
+            partia = partie.get(posel).partia;
+        }
+        return partia;
+    }
+
+    private static Wystapienie createWystapienie(Date date, String posel, String stanowisko, String tresc, String tytul, String partia) {
+        Wystapienie wystapienie = new Wystapienie();
+        wystapienie.setPosel(posel);
+        wystapienie.setData(date);
+        wystapienie.setPartia(partia);
+        wystapienie.setStanowisko(stanowisko);
+        wystapienie.setTresc(tresc);
+        wystapienie.setTytul(tytul);
+        return wystapienie;
+    }
+
+    private static String getTytul(String baseTitle, String poselFull) {
+        String titleSuffix = "";
+        if (poselFull != null) {
+            titleSuffix = " " + poselFull;
+        }
+        return baseTitle.substring(0, baseTitle.length() - 1) + titleSuffix;
+    }
+
+    private static Hashtable<String, String> getPoslowie(String header) {
+        List<Element> osoby = JOOX.$(header).find("person").get();
+        Hashtable<String, String> poslowie = new Hashtable<String, String>(osoby.size());
+        for (Element posel : osoby) {
+            String persName = JOOX.$(posel).find("persName").first().content();
+            String attr = JOOX.$(posel).attr("id");
+            poslowie.put(attr, persName);
+        }
+        return poslowie;
+    }
+
+    private static Date getDate(String header) {
+        String dateString = JOOX.$(header).find("sourceDesc").find("bibl")
+                .find("date").first().content();
+        DateFormat dateFormatter = new SimpleDateFormat("YYYY-MM-DD");
+        Date parse = null;
+        try {
+            parse = dateFormatter.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return parse;
+    }
+
+    private static String getBaseTitle(String header) {
+        String baseTitle = "";
+        List<Element> titles = JOOX.$(header).find("sourceDesc").find("bibl").find("title").get();
+        for (Element titlElem : titles)
+            if (titlElem.getAttribute("xml:lang").equalsIgnoreCase("pl"))
+                baseTitle = titlElem.getTextContent();
+        return baseTitle;
+    }
+
+    private static String readContentFile() {
         File textFile = FileUtils.getFile("../psc-data/01-pp-001-01/text_structure.xml");
-        String header = null;
         String text = null;
         try {
-            header = FileUtils.readFileToString(headerFile);
             text = FileUtils.readFileToString(textFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return text;
+    }
 
-        List<Element> osoby = JOOX.$(header).find("person").get();
-
-        Hashtable<String, String> poslowie = new Hashtable<String, String>(osoby.size());
-
-        for (Element posel : osoby) {
-
-            String persName = JOOX.$(posel).find("persName").first().content();
-            String attr = JOOX.$(posel).attr("id");
-            poslowie.put(attr, persName);
-
+    private static String readHeaderFile() {
+        File headerFile = FileUtils.getFile("../psc-data/01-pp-001-01/header.xml");
+        String header = null;
+        try {
+            header = FileUtils.readFileToString(headerFile);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return header;
+    }
 
-        List<Element> wystapienia = JOOX.$(text).find("body").find("div").get();
-
-        for (Element wystąpienie : wystapienia) {
-
-            List<Element> wypowiedzi = JOOX.$(wystąpienie).find("u").get();
-
-            for (Element wypowiedz : wypowiedzi) {
-
-                String who = JOOX.$(wypowiedz).attr("who");
-
-                if (who != null) {
-                    if (!who.equalsIgnoreCase("#komentarz") && !who.equalsIgnoreCase("#ZebraniWstajaZMiejscISpiewaja")) {
-
-                        System.out.println(poslowie.get(who.replace("#","")) + ": " + JOOX.$(wypowiedz).content());
-
-                    } else {
-
-
-
-                    }
-                }
-            }
-
+    private static HashMap<String, OpisPoslaHelper> parsePartiesList(ArrayList partiesList) {
+        HashMap<String, OpisPoslaHelper> partie = new HashMap<String, OpisPoslaHelper>(partiesList.size());
+        for (Object entry : partiesList) {
+            String key = ((HashMap<String, String>) entry).get("posel");
+            OpisPoslaHelper helper = new OpisPoslaHelper();
+            helper.stanowisko = ((HashMap<String, String>) entry).get("stanowisko");
+            helper.partia = ((HashMap<String, String>) entry).get("partia");
+            helper.objecie_stanowiska = ((HashMap<String, String>) entry).get("objecie_stanowiska");
+            partie.put(key, helper);
         }
+        return partie;
+    }
+
+    private static ArrayList readPartiesListFile(int kadencja) {
+        File partiesFile = FileUtils.getFile("../psc-data/poslowie_" + kadencja + ".json");
+        ArrayList partiesList = new ArrayList();
+        try {
+            partiesList = JsonProcessor.readFromFile(partiesFile, partiesList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return partiesList;
+    }
+
+    private static class OpisPoslaHelper {
+        String stanowisko;
+        String partia;
+        String objecie_stanowiska;
+    }
+
+    private static class PoselStanowiskoHelper {
+        String stanowisko;
+        String posel;
     }
 
 }
