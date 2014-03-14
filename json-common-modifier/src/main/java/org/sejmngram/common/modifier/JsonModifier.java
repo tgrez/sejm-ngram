a83cfc03d0105c0ffc023851640ec845648ee9b9
Krzysztof Siejkowski <krzysztof.siejkowski@ext.allegro.pl>
2014-03-14 19:24:23 +0100

    JsonModifier project added.

---
+++ json-common-modifier/src/main/java/org/sejmngram/common/modifier/JsonModifier.java
@@ -0,0 +1,15 @@
+package org.sejmngram.common.modifier;
+
+/**
+ * Created by krzysztofsiejkowski on 13/03/14.
+ */
+public class JsonModifier {
+
+    public static void main( String[] args ) {
+        modifyJson();
+    }
+
+    private static void modifyJson() {
+
+    }
+}
