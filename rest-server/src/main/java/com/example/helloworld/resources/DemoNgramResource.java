f6d3df4ec893b64ce60d52bee268ee202e1cfc4e
tgrez <tomasz.guz@gmail.com>
2014-03-18 20:37:26 +0100

    added logging, which actually works in Dropwizard

--- rest-server/src/main/java/com/example/helloworld/resources/DemoNgramResource.java
+++ rest-server/src/main/java/com/example/helloworld/resources/DemoNgramResource.java
@@ -1,17 +1,32 @@
 package com.example.helloworld.resources;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.sejmngram.database.fetcher.json.datamodel.NgramResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+
 import com.example.helloworld.factory.NgramProvider;
 import com.google.common.base.Optional;
 import com.yammer.metrics.annotation.Timed;
-import org.sejmngram.database.fetcher.json.datamodel.NgramResponse;
-
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 
 @Path("/api2/ngram")
 @Produces(MediaType.APPLICATION_JSON)
 public class DemoNgramResource {
 
+	private static final LoggerContext factory = (LoggerContext) LoggerFactory.getILoggerFactory();
+	private static final Logger root = factory.getLogger(DemoNgramResource.class);
+	
 	private final NgramProvider ngramProvider;
 
 	public DemoNgramResource() {
@@ -23,7 +38,14 @@ public class DemoNgramResource {
     @Timed
 	public NgramResponse sayHello(@PathParam("ngram") String ngramName,
 			@QueryParam("name") Optional<String> name) {
-		return ngramProvider.generateNgramResponse(ngramName);
+		root.error(ngramName);
+		String ngramURLdecoded;
+		try {
+			ngramURLdecoded = URLDecoder.decode(ngramName, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			ngramURLdecoded = ngramName;
+		}
+		return ngramProvider.generateNgramResponse(ngramURLdecoded);
 	}
 
 }
