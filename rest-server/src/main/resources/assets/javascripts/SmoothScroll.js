cf12904a1241353431b47b2d69c22ce23f399d64
MaciejLisCK <accounts@maciejlis.com>
2014-03-19 19:42:06 +0000

    SmoothScroll

---
+++ rest-server/src/main/resources/assets/javascripts/SmoothScroll.js
@@ -0,0 +1,12 @@
+﻿$('.smooth-scroll').on('click', function (event) {
+    event.preventDefault();
+
+    var dest = 0;
+    if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+        dest = $(document).height() - $(window).height();
+    } else {
+        dest = $(this.hash).offset().top;
+    }
+
+    $('html,body').animate({ scrollTop: dest }, 400, 'swing');
+});
