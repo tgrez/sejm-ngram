1eb646f996edd8f49eea473a0fe42a8ba424ada2
tgrez <tomasz.guz@gmail.com>
2014-04-17 23:40:27 +0200

    updated sejmometr download script

--- scripts/sejmometr/wystapienia_get.sh
+++ scripts/sejmometr/wystapienia_get.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
 
 echo "################################################################"
-echo "The script downloads and parses 'wystapienia' from http://sejmometr.pl/sejm_wystapienia/id"
+echo "The script downloads 'wystapienia' from http://sejmometr.pl/sejm_wystapienia/id"
 echo "Three arguments expected: first-id last-id outputfile"
 echo " [first-id last-id] gives range of ids to be downloaded "
-echo " outputfile denotes CSV-file for results to be stored in"
-echo "Example: sh wystapienia_get.sh 10 20 wystapienia10_20.txt"
+echo " outputfile denotes json file prefix for download result to be stored in"
+echo " default value for outputfile is wystapienie_"
+echo "Example:  sh wystapienia_get.sh 10 20 wystapienie_"
+echo "          then the first file will be saved as wystapienie_10.json"
 echo "################################################################"
 
-rm -rf $3
-echo "id\t;\tposel\t;\tstanowisko\t;\ttekst" > $3  #write CSV header
-mkdir /tmp/wystapienia
+timestamp=$(date +%Y%m%d_%H%M%S)
+dir=./wystapienia_$timestamp
+default_filename="wystapienie_"
+filename=${3:-$default_filename}
+
+mkdir $dir
+
 
 for i in $(seq $1 $2)
 do
-    echo "-------------------------"
-    echo "Downloading with id=$i..."
-    if wget http://sejmometr.pl/sejm_wystapienia/$i -q -O /tmp/wystapienia/wystapienie$i.html; then
-
-        echo "Parsing HTML with id=$i to CSV file..."
-        echo -n "$i\t;\t" >> $3 # append id
-        if python wystapienia_parse.py < /tmp/wystapienia/wystapienie$i.html >> $3; then #append parsed data
-            echo "HTML for id=$i parsed properly."
-        else
-            echo "Failure while parsing HTML for id=$i. Breaking..."
-            break
-        fi
-
-        rm -f /tmp/wystapienia/wystapienie$i.html
-
-    else
-        echo "Failure while downloading id=$i. Breaking..."
-        break            
-    fi
+    fullpath=$dir/$filename$i.json
+    echo "--------------------------------------------------------------------"
+    echo "Downloading with id=$i to $fullpath"
+    wget http://sejmometr.pl/sejm_wystapienia/$i.json -q -O $fullpath
 
 done
 
