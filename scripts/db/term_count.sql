442afb3a3a38ba34e595fe3495208d8b5c1ff55e
MaciejLisCK <accounts@maciejlis.com>
2014-05-11 18:40:49 +0200

    Create term_count.sql

---
+++ scripts/db/term_count.sql
@@ -0,0 +1,13 @@
+USE `sejmngram`;
+DROP function IF EXISTS `term_count`;
+
+DELIMITER $$
+USE `sejmngram`$$
+CREATE FUNCTION `term_count` (str TEXT, term TEXT)
+RETURNS INTEGER
+BEGIN
+	RETURN LENGTH(str) - LENGTH(REPLACE(str, term, ''));
+END$$
+
+DELIMITER ;
+
