f468f0405f73fc7a62bfd18e217f0117cb17d9e3
MaciejLisCK <accounts@maciejlis.com>
2014-05-11 18:47:11 +0200

    Update term_count.sql

--- scripts/db/term_count.sql
+++ scripts/db/term_count.sql
@@ -6,7 +6,7 @@ USE `sejmngram`$$
 CREATE FUNCTION `term_count` (str TEXT, term TEXT)
 RETURNS INTEGER
 BEGIN
-	RETURN LENGTH(str) - LENGTH(REPLACE(str, term, ''));
+	RETURN (LENGTH(str) - LENGTH(REPLACE(str, term, '')))/LENGTH(term);
 END$$
 
 DELIMITER ;
