111e28ae98b7357b0096d2258d0c5868c5f243ae
tgrez <tomasz.guz@gmail.com>
2014-05-18 18:44:15 +0200

    cleaning: imports

--- db-fetcher-mysql-fts/src/main/java/org/sejmngram/database/fetcher/connection/MySqlFtsDbConnector.java
+++ db-fetcher-mysql-fts/src/main/java/org/sejmngram/database/fetcher/connection/MySqlFtsDbConnector.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Types;
 import java.util.ArrayList;
 
 import org.sejmngram.database.fetcher.converter.IdConverter;
