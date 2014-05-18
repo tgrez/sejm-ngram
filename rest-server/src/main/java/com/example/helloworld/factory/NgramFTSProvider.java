1e55070e160b647bfc9234b8d78ef08b862bcb4e
tgrez <tomasz.guz@gmail.com>
2014-05-18 15:26:36 +0200

    cleanings: removed unused method

--- rest-server/src/main/java/com/example/helloworld/factory/NgramFTSProvider.java
+++ rest-server/src/main/java/com/example/helloworld/factory/NgramFTSProvider.java
@@ -13,26 +13,6 @@ public class NgramFTSProvider {
         db.connect();
     }
 
-
-    public NgramResponse generateDefaultNgramResponse(String ngramName) {
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        List<PartiesNgrams> partiesNgrams = new ArrayList<PartiesNgrams>();
-
-        for (int j = 0; j < RandomUtils.nextInt(9)+ 1 ; j++) {
-
-            List<ListDate> listDates = new ArrayList<ListDate>();
-
-            for (int i = 0; i < RandomUtils.nextInt(200); i++) {
-                long beginDate = 689835600000l;
-                listDates.add(new ListDate(sdf.format(new Date(beginDate + RandomUtils.nextInt())), RandomUtils.nextInt((200))));
-            }
-
-            partiesNgrams.add(new PartiesNgrams("Party" + j, listDates));
-        }
-        return new NgramResponse(ngramName, partiesNgrams);
-    }
-
     public NgramResponse generateNgramResponse(String ngramName) {
         return db.retrieve(ngramName);
     }
