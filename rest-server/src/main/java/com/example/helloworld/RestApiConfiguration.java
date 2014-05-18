1ef4b93ed4c3cbe63102a4cff3d2d6452d54d4eb
tgrez <tomasz.guz@gmail.com>
2014-05-18 19:39:15 +0200

    changed configuration class

--- rest-server/src/main/java/com/example/helloworld/RestApiConfiguration.java
+++ rest-server/src/main/java/com/example/helloworld/RestApiConfiguration.java
@@ -5,17 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class RestApiConfiguration extends Configuration {
-    @NotEmpty
-    @JsonProperty
-    private String template;
 
     @NotEmpty
     @JsonProperty
     private String defaultName = "Stranger";
-
-    public String getTemplate() {
-        return template;
-    }
 
     public String getDefaultName() {
         return defaultName;
