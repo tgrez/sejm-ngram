fedebc30b8d29e4b3de11c1d50868b771fa2f878
Michał Siemiończyk <michal.siemionczyk@elpassion.pl>
2014-06-15 23:38:11 +0200

    Revert "fixing bug with null property"
    This reverts commit db692c8b3f5686ac172f329d751d36a06efd64c8.

--- rest-server/src/main/resources/assets/javascripts/viz.js
+++ rest-server/src/main/resources/assets/javascripts/viz.js
@@ -10,7 +10,6 @@ function visualize(term) {
         var colors = d3.scale.category10();
 
         var slugify = function (s) {
-            if (!s) return "";
             return s.toLowerCase().replace(/[^a-z0-9-]/g, "-")
         }
 
