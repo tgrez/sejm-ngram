c1c5ebfbe9ff4e2a255ec85ff6f02b0dbe232767
Michał Siemiończyk <michal.siemionczyk@elpassion.pl>
2014-10-15 21:18:17 +0200

    pre-changes of michal's script to generate common jsons from sejmometr api

---
+++ sejmometr-client/src/main/java/org/sejmngram/sejmometr/CommonFromFilesApp.java
@@ -0,0 +1,19 @@
+package org.sejmngram.sejmometr;
+
+import org.sejmngram.sejmometr.transform.TransformExecutor;
+
+/**
+ * Created by michalsiemionczyk on 09/10/14.
+ */
+public class CommonFromFilesApp {
+
+    public static void main(String[] args) {
+
+
+        TransformExecutor.process(responses);
+
+
+        System.out.println("2!");
+
+    }
+}
