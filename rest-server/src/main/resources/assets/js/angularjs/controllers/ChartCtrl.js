3ba5ecca03df266065496ff3aa1e8d7c70e31441
Michał Siemiończyk <michal.siemionczyk@elpassion.pl>
2015-04-18 16:15:58 +0200

    temporarily changed hostname to work on it locally

--- rest-server/src/main/resources/assets/js/angularjs/controllers/ChartCtrl.js
+++ rest-server/src/main/resources/assets/js/angularjs/controllers/ChartCtrl.js
@@ -36,7 +36,8 @@ module.controller('ChartCtrl', function ($scope, $http, $window, $routeParams, $
         for (var i = 0; i < phrases.length; i++) {
             $scope.search.callsInProgressCount++;
 
-            d3.json("service/api/ngramfts/" + phrases[i].text,
+//            d3.json("service/api/ngramfts/" + phrases[i].text,
+            d3.json("http://localhost:8080/service/api/ngramfts/" + phrases[i].text,
                 function (error, data) {
                     $scope.search.callsInProgressCount--;
 
