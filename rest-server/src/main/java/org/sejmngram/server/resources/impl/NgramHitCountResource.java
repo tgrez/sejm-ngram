08a1561aa270fec78d374cfd9583d184d13482d5
tgrez <tomasz.guz@gmail.com>
2015-07-14 22:41:47 +0200

    stub results for top hits

--- rest-server/src/main/java/org/sejmngram/server/resources/impl/NgramHitCountResource.java
+++ rest-server/src/main/java/org/sejmngram/server/resources/impl/NgramHitCountResource.java
@@ -2,6 +2,7 @@ package org.sejmngram.server.resources.impl;
 
 import io.dropwizard.jersey.params.IntParam;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,13 +20,17 @@ import com.google.common.base.Optional;
 public class NgramHitCountResource implements HitCountResource {
 
     private final Optional<? extends HitCounter> counter;
+    private static final Set<String> STUB_VALUES =
+            new HashSet<String>(Arrays.asList("rosja", "putin", "deficyt", "aborcja", "tusk", "euro"));
 
     public NgramHitCountResource(Optional<? extends HitCounter> counter) {
         this.counter = counter;
     }
 
     public Set<String> getTopNgram(IntParam limit) {
-        if (counter.isPresent() && limit.get() != null) {
+        if (!counter.isPresent()) {
+            return STUB_VALUES;
+        } else if (limit.get() != null) {
             return counter.get().getTop(limit.get());
         } else {
             return new HashSet<String>();
