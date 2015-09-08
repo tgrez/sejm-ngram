fc9c92589a0d0a2348d2bd4fb75f6c06e0cddbf9
Michał Siemiończyk <michal.siemionczyk@elpassion.pl>
2015-09-08 20:09:51 +0200

    removed some unnecessary code

--- rest-server/src/main/resources/assets/js/angularjs/directives/GraphLegend.js
+++ rest-server/src/main/resources/assets/js/angularjs/directives/GraphLegend.js
@@ -4,16 +4,6 @@
 'use strict';
 
 module
-.controller('LegendController', ['$scope', function($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
-}])
 .directive('stGraphLegend', function() {
     return {
        restrict: 'E',
