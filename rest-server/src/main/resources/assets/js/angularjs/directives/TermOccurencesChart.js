2cbe7d7438528c78ddc8e7ad1d06f1194904ccde
Kamil Dworakowski <kamil@dworakowski.name>
2015-10-15 22:46:24 +0200

    refactor chart some more

--- rest-server/src/main/resources/assets/js/angularjs/directives/TermOccurencesChart.js
+++ rest-server/src/main/resources/assets/js/angularjs/directives/TermOccurencesChart.js
@@ -13,30 +13,80 @@ module.directive('stTermOccurencesChart', function () {
             displayRange: '=',
         },
         link: function link(scope, iElement, iAttrs) {
+						var chartPane = {};
+
             var LINE_PREFIX = 'termOccurencesLine'
-            var svg;
-            var svgWidth;
-            var svgHeight;
-            var chartWidth;
-            var chartHeight;
-            var linesCanvasWidth;
-            var linesCanvasHeight;
-            var defs;
-            var clipPath;
-            var clipPathRegion;
-            var chart;
-            var linesCanvas;
-            var axisX;
-            var axisY;
-            var scaleX;
-            var scaleY;
-
-            scope.isInitialized = false;
-
 
             scope.$watch('termsOccurences.length', onDataChange);
             scope.$watch('displayRange', onDisplayRangeChange);
             scope.$watch('partiesNames', onPartiesVisibilityChange, true)
+
+            scope.isInitialized = false;
+
+						function initialize() {
+								var chartMargin = {
+										top: 10,
+										bottom: 10,
+										left: 10,
+										right: 10
+								};
+								var linesCanvasMargin = {
+										top: 40,
+										bottom: 40,
+										left: 40,
+										right: 40
+								};
+								var svg = d3.select('#term-occurences-chart');
+								var svgWidth = svg.node().offsetWidth;
+								var svgHeight = svg.node().offsetHeight;
+								var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+								var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+								var linesCanvasWidth = chartWidth - linesCanvasMargin.left - linesCanvasMargin.right;
+								var linesCanvasHeight = chartPane.linesCanvasHeight =
+										chartHeight - linesCanvasMargin.top - linesCanvasMargin.bottom;
+
+								var defs = svg.append('defs');
+								var clipPath = defs.append('clipPath')
+										.attr('id', 'termOccurencesLinesCanvasRegion');
+								var clipPathRegion = clipPath.append('rect');
+								clipPathRegion.attr({
+										width: linesCanvasWidth,
+										height: linesCanvasHeight
+								});
+
+								var chart = svg.append('g')
+										.attr({
+												'class': 'chart',
+												'width': chartWidth,
+												'height': chartHeight,
+												'transform': 'translate(' + chartMargin.left + ', ' + chartMargin.top + ')'
+										});
+
+								chartPane.linesCanvas = chart.append('g')
+										.attr({
+												'class': 'linesCanvas',
+												'width': linesCanvasWidth,
+												'height': linesCanvasHeight,
+												'transform': 'translate(' + linesCanvasMargin.left + ', ' + linesCanvasMargin.top + ')'
+										});
+								chartPane.axisX = chart.append('g')
+										.attr({
+												'class': 'axis axisX',
+												'transform': 'translate(' + linesCanvasMargin.left + ', ' + (linesCanvasHeight + linesCanvasMargin.top) + ')'
+										});
+								chartPane.axisY = chart.append('g')
+										.attr({
+												'class': 'axis axisY',
+												'transform': 'translate(' + linesCanvasMargin.left + ', ' + linesCanvasMargin.top + ')'
+										});
+
+								chartPane.scaleX = d3.time.scale()
+										.range([0, linesCanvasWidth]);
+								chartPane.scaleY = d3.scale.linear()
+										.range([linesCanvasHeight, 0]);
+
+								scope.isInitialized = true;
+						}
 
             function onPartiesVisibilityChange(){
                 scope.graphDrawHelper.setLineVisibility(LINE_PREFIX)
@@ -53,75 +103,12 @@ module.directive('stTermOccurencesChart', function () {
                     update();
                 }
 
-                function initialize() {
-                    var chartMargin = {
-                        top: 10,
-                        bottom: 10,
-                        left: 10,
-                        right: 10
-                    };
-                    var linesCanvasMargin = {
-                        top: 40,
-                        bottom: 40,
-                        left: 40,
-                        right: 40
-                    };
-                    svg = d3.select('#term-occurences-chart');
-                    svgWidth = svg.node().offsetWidth;
-                    svgHeight = svg.node().offsetHeight;
-                    chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-                    chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
-                    linesCanvasWidth = chartWidth - linesCanvasMargin.left - linesCanvasMargin.right;
-                    linesCanvasHeight = chartHeight - linesCanvasMargin.top - linesCanvasMargin.bottom;
-                    
-                    defs = svg.append('defs');
-                    clipPath = defs.append('clipPath')
-                        .attr('id', 'termOccurencesLinesCanvasRegion');
-                    clipPathRegion = clipPath.append('rect');
-                    clipPathRegion.attr({
-                        width: linesCanvasWidth,
-                        height: linesCanvasHeight
-                    });
-
-                    chart = svg.append('g')
-                        .attr({
-                            'class': 'chart',
-                            'width': chartWidth,
-                            'height': chartHeight,
-                            'transform': 'translate(' + chartMargin.left + ', ' + chartMargin.top + ')'
-                        });
-
-                    linesCanvas = chart.append('g')
-                        .attr({
-                            'class': 'linesCanvas',
-                            'width': linesCanvasWidth,
-                            'height': linesCanvasHeight,
-                            'transform': 'translate(' + linesCanvasMargin.left + ', ' + linesCanvasMargin.top + ')'
-                        });
-                    axisX = chart.append('g')
-                        .attr({
-                            'class': 'axis axisX',
-                            'transform': 'translate(' + linesCanvasMargin.left + ', ' + (linesCanvasHeight + linesCanvasMargin.top) + ')'
-                        });
-                    axisY = chart.append('g')
-                        .attr({
-                            'class': 'axis axisY',
-                            'transform': 'translate(' + linesCanvasMargin.left + ', ' + linesCanvasMargin.top + ')'
-                        });
-
-                    scaleX = d3.time.scale()
-                        .range([0, linesCanvasWidth]);
-                    scaleY = d3.scale.linear()
-                        .range([linesCanvasHeight, 0]);
-
-                    scope.isInitialized = true;
-                }
             }
 
             function onDisplayRangeChange() {
                 if (scope.isInitialized) {
-                    scaleX.domain(scope.displayRange);
-										redrawLines(scope.multiLineData);
+                    chartPane.scaleX.domain(scope.displayRange);
+										redrawLines(scope.multiLineData, false);
                 }
             }
 
@@ -142,59 +129,59 @@ module.directive('stTermOccurencesChart', function () {
 
                 var isDisplayRangeValid = typeof scope.displayRange !== 'undefined' && scope.displayRange !== null && scope.displayRange.length > 0;
                 if (isDisplayRangeValid)
-                    scaleX.domain(scope.displayRange);
+                    chartPane.scaleX.domain(scope.displayRange);
                 else 
-                    scaleX.domain(xRange);
-                scaleY.domain(yRange);
+                    chartPane.scaleX.domain(xRange);
+                chartPane.scaleY.domain(yRange);
 
-								redrawLines(multiLineData);
+								redrawLines(multiLineData, true);
 
                 var axisXFunction = d3.svg.axis()
-                    .scale(scaleX)
+                    .scale(chartPane.scaleX)
                     .orient('bottom')
                     .ticks(4);
                 var axisYFunction = d3.svg.axis()
-                    .scale(scaleY)
+                    .scale(chartPane.scaleY)
                     .orient('left');
 
-                axisX.transition().duration(1000).call(axisXFunction);
-                axisY.transition().duration(1000).call(axisYFunction);
+                chartPane.axisX.transition().duration(1000).call(axisXFunction);
+                chartPane.axisY.transition().duration(1000).call(axisYFunction);
             }
 
-						function redrawLines(multiLineData) {
+						function redrawLines(multiLineData, shouldAnimate) {
                 for (var i = 0; i < multiLineData.length; i++) {
                     var lineId = scope.graphDrawHelper.generateLineId(LINE_PREFIX, multiLineData[i].lineName);
 
                     var lineFunction = d3.svg.line()
-                        .x(function (o) { return scaleX(o.date); })
-                        .y(function (o) { return scaleY(o.count); });
+                        .x(function (o) { return chartPane.scaleX(o.date); })
+                        .y(function (o) { return chartPane.scaleY(o.count); });
                     var flatLineFunction = d3.svg.line()
-                        .x(function (o) { return scaleX(o.date); })
-                        .y(function (o) { return linesCanvasHeight; });
+                        .x(function (o) { return chartPane.scaleX(o.date); })
+                        .y(function (o) { return chartPane.linesCanvasHeight; });
 
 
                     var line = d3.select('#' + lineId);
                     var isLineExist = line.empty();
                     if (isLineExist) {
-                        line = linesCanvas.append("path")
+                        line = chartPane.linesCanvas.append("path")
                             .attr('id', lineId)
                             .attr('class', 'line')
                             .attr('clip-path', 'url(#termOccurencesLinesCanvasRegion)')
-                            .attr('style', 'stroke: ' + scope.graphDrawHelper.generateLineColorForPartyName(multiLineData[i].lineName))
-                            .attr('d', flatLineFunction(multiLineData[i].occurences))
-                            .transition()
-                            .duration(1000)
-                            .attr('d', lineFunction(multiLineData[i].occurences));
+                            .attr('style', 'stroke: ' + scope.graphDrawHelper.generateLineColorForPartyName(multiLineData[i].lineName));
+												if (shouldAnimate) {
+                            line.attr('d', flatLineFunction(multiLineData[i].occurences))
+															.transition()
+															.duration(1000)
+												}
+                        line.attr('d', lineFunction(multiLineData[i].occurences));
                     } else {
-                        line.transition()
-                            .duration(1000)
-                            .attr('d', lineFunction(multiLineData[i].occurences));
+												if (shouldAnimate) line.transition().duration(1000);
+												line.attr('d', lineFunction(multiLineData[i].occurences));
                     }
                 }
 
-                scope.graphDrawHelper.removeObsolateLines(linesCanvas, multiLineData, LINE_PREFIX);
-
-								}
+                scope.graphDrawHelper.removeObsolateLines(chartPane.linesCanvas, multiLineData, LINE_PREFIX);
+						}
         },
         template:
             '<svg width="758" height="400">' +
