2c8b90ace5c26e6325efff3ba4f1eae9e1e4e9cc
Kamil Dworakowski <kamil@dworakowski.name>
2015-10-20 21:30:22 +0200

    fix build

--- db-fetcher-api/src/main/java/org/sejmngram/database/fetcher/json/datamodel/NgramResponse.java
+++ db-fetcher-api/src/main/java/org/sejmngram/database/fetcher/json/datamodel/NgramResponse.java
@@ -23,11 +23,6 @@ public class NgramResponse {
         this.partiesNgrams = partiesNgrams;
     }
 
-    public NgramResponse(String ngram) {
-        this.ngram = ngram;
-    }
-
-
     public List<PartiesNgrams> getPartiesNgrams() {
         return partiesNgrams;
     }
