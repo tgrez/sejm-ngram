d7d7c315a1b4e4fc311dab1a4a0f1912419a671d
Kamil Dworakowski <kamil@dworakowski.name>
2015-10-22 14:04:23 +0200

    improve the elasticsearch import script

--- scripts/elasticsearch/build_index_main.py
+++ scripts/elasticsearch/build_index_main.py
@@ -2,11 +2,16 @@
 
 import os
 import json
+import sys
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch()
 
-for base, subdirs, files in os.walk('/home/tomasz/workspace/sejmAPI/jsonModifiedData/processed_psc/1'):
+if not (len(sys.argv) == 2):
+    print "USAGE:", sys.argv[0], "<path to dir with .json files>"
+    sys.exit(1)
+
+for base, subdirs, files in os.walk(sys.argv[1]):
     for name in files:
         if name.endswith('.json'):
             path = base + '/' + name
