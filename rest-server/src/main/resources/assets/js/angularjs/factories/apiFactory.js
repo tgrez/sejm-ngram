81076b029b376a13a986f2391c2758e9a36c89a0
Michał Siemiończyk <michal.siemionczyk@elpassion.pl>
2015-12-08 22:30:50 +0100

    close (closes) issue https://github.com/tgrez/sejm-ngram/issues/88

--- rest-server/src/main/resources/assets/js/angularjs/factories/apiFactory.js
+++ rest-server/src/main/resources/assets/js/angularjs/factories/apiFactory.js
@@ -9,6 +9,7 @@ module.factory('apiFactory', function( $http, apiMockFactory ) {
 
   dataFactory.getAllWordsPerDate = function() {
     return $http.get('/data/wordcount_per_date.json');
+    return  $http.get('/data/wordcount_per_date.json');
   }
 
   dataFactory.getGraphNgram = function (phrase) {
